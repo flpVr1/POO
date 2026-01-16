f161ef49926a9e59d961bc594a417e50dfdf23bc
flpVr1 <felipevergaraj7@gmail.com>
2026-01-15 21:18:39 -0300

    Primeros pasos para POO

---
+++ oop_python.py
@@ -0,0 +1,38 @@
+class Personaje:
+    # Contructor
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
+        # Atributos
+        self.nombre = nombre
+        self.fuerza = fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida
+
+    def atributos(self):
+        print(self.nombre, ":", sep=" ")
+        print("• Fuerza:", self.fuerza)
+        print("• Inteligencia:", self.inteligencia)
+        print("• Defensa:", self.defensa)
+        print("• Vida:", self.vida)
+
+    def subir_nivel(self, fuerza, inteligencia, defensa):
+        self.fuerza = self.fuerza + fuerza
+        self.inteligencia = self.inteligencia + inteligencia
+        self.defensa = self.defensa + defensa
+
+    def esta_vivo(self):
+        return self.vida > 0
+    
+    def morir(self):
+        self.vida = 0
+        print(self.nombre, "Ha muerto")
+
+    def dano(self, enemigo):
+        return self.fuerza - enemigo.defensa
+    
+    def atacar(self, enemigo):
+        return self.fuerza - enemigo.defensa
+
+mi_personaje = Personaje('Felipe', 10, 1, 5, 100)
+mi_enemigo = Personaje("Enemigo Stando", 8, 5, 3, 100)
+print(mi_personaje.dano(mi_enemigo))
